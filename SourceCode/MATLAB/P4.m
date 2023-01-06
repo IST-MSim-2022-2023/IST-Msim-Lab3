dc711847e49996c21a1b763a4fe9deb4a048084c
13A! <maria.teresa.ramos.nogueira@tecnico.ulisboa.pt>
2023-01-06 23:16:32 +0000

    Added P4 cutie :3
    Adoro-te

--- SourceCode/MATLAB/P4.m
+++ SourceCode/MATLAB/P4.m
@@ -1,18 +1,27 @@
-%% P2
+%% P4
 %% clear->erase workspace variables, clc->clean command window, close all-> close all currently open figures
 clear; clc; close all;
 
 %% variables
 
-% terreno
-Vy_Inicial = 1; Treshold_y = 10;
-K1 = -1; K2 = 0.8;
-
 %init
 Altura = 10;
-V = 15;
+V = 30;
+K = -0.8;
 
 %% run Sim
-out = sim("P4simulink", 20);
+out = sim("P4simulink2022a", 8.5);
 
-plot(out.y,out.Altura,LineStyle='-', LineWidth=3, Color="#EDB120"); hold on
+%% Plot
+set(gcf, 'Position',  [100, 100, 680, 500])
+plot(out.y,out.Altura,LineStyle='--', LineWidth=1.5, Color="#745296"); hold on
+
+plot(out.y(end),out.Altura(end),'-o', LineWidth=3.5, Color="#D1B1CB", MarkerFaceColor="#D1B1CB"); hold on
+
+xline(0, '-', 'Parede 1', 'interpreter','latex', 'LineWidth',2,'FontSize',9,"Color","#252323"); 
+hold on
+xline(5, '-', 'Parede 2', 'interpreter','latex', 'LineWidth',2,'FontSize',9,"Color","#252323"); 
+hold off
+
+xlabel('\textbf{$\mathbf{y}$ [m]}','interpreter','latex','FontSize',16)
+ylabel('\textbf{$\mathbf{z}$ [m]}','interpreter','latex','FontSize',16);
