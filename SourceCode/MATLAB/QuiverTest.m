f7f875f462950083f793b776fa7af2567c36d861
João Gonçalves <81173835+eusouojoao@users.noreply.github.com>
2023-01-13 16:39:06 +0000

    *

--- SourceCode/MATLAB/QuiverTest.m
+++ SourceCode/MATLAB/QuiverTest.m
@@ -1,17 +1,20 @@
 g = 9.81;
 
 [X,Y] = meshgrid(0:2:38,-19:2:19);
-xTresh = 0.1;
-yTresh = -0.1;
+% xTresh = 0.1;
+% yTresh = -0.1;
 U = Y;
 V = zeros(length(U),length(U)) -g;
 
-xacceptIndex = X >= xTresh;
-yacceptIndex = Y >= yTresh;
+% xacceptIndex = X >= xTresh;
+% yacceptIndex = Y >= yTresh;
 
-quiver(Y(yacceptIndex),X(yacceptIndex),V(yacceptIndex),U(yacceptIndex),LineWidth=0.01, Color = "#5AA4FF"); hold on;
-quiver(Y(xacceptIndex),X(xacceptIndex),V(xacceptIndex),U(xacceptIndex),LineWidth=0.01, Color="#5AA4FF"); 
+% quiver(Y(yacceptIndex),X(yacceptIndex),V(yacceptIndex),U(yacceptIndex),LineWidth=0.01, Color = "#5AA4FF"); hold on;
+% quiver(Y ,X ,V ,U ,LineWidth=0.01, Color="#5AA4FF"); 
 
+quiver(Y, X, V, U, LineWidth=1.5, Color="#5AA4FF"); 
+
+set(gca,'Visible','off')
 h=gca; h.XAxis.TickLength = [0 0];
 h=gca; h.YAxis.TickLength = [0 0];
 
